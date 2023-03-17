eea231e7f0778061ac6a65223d74c6689fe550cc
Jose Daniel Ramirez Bastidas <danik_mi@hotmail.com>
2023-03-16 22:51:14 -0700

    Update login

--- CONTROLLER/login.js
+++ CONTROLLER/login.js
@@ -1,13 +1,17 @@
-const username = document.getElementById('usrName')
-const password = document.getElementById('usrPass')
-const button = document.getElementById('button')
+const username = document.getElementById('usrName');
+const password = document.getElementById('usrPass');
+const button = document.getElementById('button');
 
 button.addEventListener('click', (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const data = {
         username: username.value,
         password: password.value
-    }
+    };
 
-    console.log(data)
-})
+    if (data.username === "Bastidas" && data.password === "123") {
+        window.location.href = "VIEW/dashboard.html";
+    } else {
+        alert("Usuario o contraseña incorrectos");
+    }
+});

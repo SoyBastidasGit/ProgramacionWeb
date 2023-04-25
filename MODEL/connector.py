3a2947b85cfa498723d9ffb15b95c1dd2ae9aa97
Jose Daniel Ramirez Bastidas <danik_mi@hotmail.com>
2023-04-24 18:09:37 -0700

    update

---
+++ MODEL/connector.py
@@ -0,0 +1,14 @@
+
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://SoyBastidas:<SoyBastidas123>@erp.a1ztdjx.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)

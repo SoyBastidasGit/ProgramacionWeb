4454bfdffb13134a49e35c43c23ee25dd7c0ec23
SoyBastidasGit <jose.ramirez18@tectijuana.edu.mx>
2023-04-28 13:46:25 -0700

    update

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,8 +1,12 @@
+const express = require("express");
+
+const app = express();
+
 const localStrategy = require('passport-local').Strategy;
 
 const User = require('../app/models/user');
 
-module.exports = function (passport) {
+module.exports = async function (passport) {
     passport.serializeUser(function (user, done) {
         done(null, user.id);
     }); 
@@ -14,13 +18,13 @@ module.exports = function (passport) {
     });
 
 //login
-    passport.use('local-login', new localStrategy({
+ /*    passport.use('local-login', new localStrategy({
        usernameField: 'user',
        passwordField: 'password',
        passReqToCallback: true 
     },
-    async function (req, user, password, done) {
-        await User.findOne({'local.user': user}, function (err, user) {
+    function (req, user, password, done) {
+        User.findOne({'local.user': user}, function (err, user) {
             if (err) { return done(err); }
             if (!user) {
                 return done(null, false, req.flash('loginMessage', 'El usuario no existe.'));
@@ -30,5 +34,25 @@ module.exports = function (passport) {
             }
             return done(null, user);
         });
-    }));
+    })); */
+
+    app.post("/login", async function(req, res){
+        try {
+            // check if the user exists
+            const user = await User.findOne({ username: req.body.username });
+            if (user) {
+              //check if password matches
+              const result = req.body.password === user.password;
+              if (result) {
+                res.render("Dashboard");
+              } else {
+                res.status(400).json({ error: "password doesn't match" });
+              }
+            } else {
+              res.status(400).json({ error: "User doesn't exist" });
+            }
+          } catch (error) {
+            res.status(400).json({ error });
+          }
+    });
 }
